fd1e2eb20e31c74d1e3e5b5b767a380842c65dc2
Alison Chan <alison.chan@detroitlabs.com>
2018-10-08 16:30:21 -0400

    end of section 6

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css'
-import './index.css';
-import App from './app/layout/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import App from "./app/layout/App";
+import * as serviceWorker from "./serviceWorker";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 let render = () => {
-    ReactDOM.render(<App />, root)
-}
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    root
+  );
+};
 
 if (module.hot) {
-    module.hot.accept('./app/layout/App', () => {
-        setTimeout(render)
-    })
+  module.hot.accept("./app/layout/App", () => {
+    setTimeout(render);
+  });
 }
 
 render();

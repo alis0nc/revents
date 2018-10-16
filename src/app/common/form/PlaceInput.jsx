cb5cf1348e0e162b6aacc1b70dc1e4b968ea624b
Alison Chan <alison.chan@detroitlabs.com>
2018-10-16 14:53:01 -0400

    end of section 10

---
+++ src/app/common/form/PlaceInput.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Form, Label } from "semantic-ui-react";
+import Script from "react-load-script";
+import PlacesAutocomplete from "react-places-autocomplete";
+
+const styles = {
+  autocompleteContainer: {
+    zIndex: 1000
+  }
+};
+
+class PlaceInput extends Component {
+  state = {
+    address: "",
+    scriptLoaded: false
+  };
+
+  onChange = address => this.setState({ address });
+
+  handleScriptLoad = () => this.setState({ scriptLoaded: true });
+
+  render() {
+    const {
+      input,
+      width,
+      onSelect,
+      placeholder,
+      options,
+      meta: { touched, error }
+    } = this.props;
+    return (
+      <Form.Field error={touched && !!error} width={width}>
+        <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFSxO1FrLdi8TBOxI6DrERPJ_knGWtxns&libraries=places"
+          onLoad={this.handleScriptLoad}
+        />
+        {this.state.scriptLoaded && (
+          <PlacesAutocomplete
+            inputProps={{ ...input, placeholder }}
+            options={options}
+            onSelect={onSelect}
+            styles={styles}
+          />
+        )}
+        {touched &&
+          error && (
+            <Label basic color="red">
+              {error}
+            </Label>
+          )}
+      </Form.Field>
+    );
+  }
+}
+
+export default PlaceInput;
--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
+import Script from "react-load-script";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
+import GoogleMapReact from "google-map-react";
+
 import { incrementCounter, decrementCounter } from "./TestActions";
 
 const mapState = state => ({
@@ -12,15 +19,75 @@ const actions = {
   decrementCounter
 };
 
+const Marker = () => <Icon name='marker' size='big' color='red' />
+
 class TestComponent extends Component {
+  static defaultProps = {
+    center: {
+      lat: 42.3190342,
+      lng: -83.1042411
+    },
+    zoom: 15
+  };
+
+  state = {
+    address: "",
+    scriptLoaded: false
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+
+    geocodeByAddress(this.state.address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => console.log("Success", latLng))
+      .catch(error => console.error("Error", error));
+  };
+
+  onChange = address => this.setState({ address });
+
+  handleScriptLoad = () => this.setState({ scriptLoaded: true });
+
   render() {
+    const inputProps = {
+      value: this.state.address,
+      onChange: this.onChange
+    };
     const { incrementCounter, decrementCounter, data } = this.props;
     return (
       <div>
+        {/* <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyDFSxO1FrLdi8TBOxI6DrERPJ_knGWtxns&libraries=places"
+          onLoad={this.handleScriptLoad}
+        /> */}
         <h1>Test Component</h1>
         <h3>The answer is {data}</h3>
         <Button onClick={decrementCounter} color="red" content="-" />
         <Button onClick={incrementCounter} color="green" content="+" />
+        <br />
+        <br />
+        <form onSubmit={this.handleFormSubmit}>
+          {this.state.scriptLoaded && (
+            <PlacesAutocomplete inputProps={inputProps} />
+          )}
+          <button type="submit">Submit</button>
+        </form>
+
+        <div style={{ height: "300px", width: "100%" }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: "AIzaSyDFSxO1FrLdi8TBOxI6DrERPJ_knGWtxns"
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+            <Marker
+              lat={42.3190342}
+              lng={-83.1042411}
+              text={"Home"}
+            />
+          </GoogleMapReact>
+        </div>
       </div>
     );
   }
